2560f145bb29357056f49fe9fb196c6c07401029
hampus <hampus.john.olausson@gmail.com>
2021-09-02 12:04:57 +0200

    Endpoints added

--- TaskManagementApp.Application/Interfaces/IGenericRepository.cs
+++ TaskManagementApp.Application/Interfaces/IGenericRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace TaskManagementApp.Application.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        
+
+        Task<T> Get(int id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<int> Add(T entity);
+
+        Task<int> Delete(int id);
+
+        Task<int> Update(T entity);
     }
 }
